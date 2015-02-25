24f09515248eb963a2d3097e58eb28e37f49776d
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-02-25 03:32:35 -0800

    a proper dotfile manager... well the starts of one

---
+++ dotty.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+COMMON = {
+	".zshrc": "~/.zshrc",
+	".nanorc": "~/.nanorx"
+}
+
+MACHINES = [
+	{
+		"name": "san",
+		"files": {
+			
+		}
+	},
+	{
+		"name": "niya",
+		"files": {
+			".i3": "~/.i3",
+			".conkyrc": "~/.conkyrc"
+		}
+	}
+	{
+		"name": "moro",
+		"files": {
+			
+		}
+	}
+]
+
+
